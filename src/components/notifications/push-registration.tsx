2301beabd84258040ff596d758b1a386e2c2bbd6
Martin Giaveno <tinchogia@gmail.com>
2026-03-26 19:20:51 -0300

    refactor: remove unused urlBase64ToUint8Array function from PushRegistration component

--- src/components/notifications/push-registration.tsx
+++ src/components/notifications/push-registration.tsx
@@ -4,17 +4,6 @@ import { useEffect } from 'react';
 
 import { subscribePushAction } from './actions';
 
-function urlBase64ToUint8Array(base64String: string): Uint8Array<ArrayBuffer> {
-  const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
-  const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
-  const rawData = window.atob(base64);
-  const outputArray = new Uint8Array(new ArrayBuffer(rawData.length));
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-  return outputArray;
-}
-
 export function PushRegistration() {
   useEffect(() => {
     if (!('serviceWorker' in navigator) || !('PushManager' in window)) return;
