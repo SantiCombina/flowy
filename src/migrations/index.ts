0f2e6b1bb353808369a7ed718f4ab95a86edb583
Giaveno <tinchogia@gmail.com>
2026-07-27 21:58:16 -0300

    fix: remove outdated migration 20260727_173043 from index

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -5,7 +5,6 @@ import * as migration_20260505_214603 from './20260505_214603';
 import * as migration_20260512_115220 from './20260512_115220';
 import * as migration_20260701_200131 from './20260701_200131';
 import * as migration_20260705_150532 from './20260705_150532';
-import * as migration_20260727_173043 from './20260727_173043';
 import * as migration_20260727_181500 from './20260727_181500';
 
 export const migrations = [
@@ -43,11 +42,6 @@ export const migrations = [
     up: migration_20260705_150532.up,
     down: migration_20260705_150532.down,
     name: '20260705_150532',
-  },
-  {
-    up: migration_20260727_173043.up,
-    down: migration_20260727_173043.down,
-    name: '20260727_173043',
   },
   {
     up: migration_20260727_181500.up,
